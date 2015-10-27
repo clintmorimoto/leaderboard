cb69918efad18ef8ef48862086a65167b581c234
clint <cmorimoto@gmail.com>
2015-10-27 10:11:25 -1000

    update from class on Tues

--- leaderboard.js
+++ leaderboard.js
@@ -1,23 +1,17 @@
+console.log("Hello World!!!");
+
+PlayersList = new Mongo.Collection('players');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  //console.log("Hello Client.");
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  //console.log("Hey there Server.");
 }
+
+Template.leaderboard.helpers({
+  'player': function(){
+    return "Some other text";
+  }
+});
